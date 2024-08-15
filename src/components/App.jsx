8bfcb3d56f7a20690e272a9e49d61c07d1aaf449
A-Cezara <alexandracezara@yahoo.ro>
2024-08-15 16:27:23 +0300

    module 4 lesson 7

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,27 +2,27 @@ import Sidebar from './Sidebar';
 import Tutors from './Tutors';
 import University from './University/University';
 import data from '../utils/data.json';
-import { Component } from 'react';
+import { useEffect } from 'react';
 // import News from './News';
 
-class App extends Component {
-  componentDidMount() {
-    localStorage.setItem('tutors', JSON.stringify(data?.tutors));
-  }
 
-  render() {
-    return (
-      <div className="wrapper">
-        <Sidebar />
-        <main className="main">
-          {/* <News /> */}
-          <h1 className="page-title">University Information</h1>
-          <University />
-          <Tutors />
-        </main>
-      </div>
-    );
-  }
+
+export default function App() {
+useEffect(() => {
+  localStorage.setItem('tutors', JSON.stringify(data?.tutors));
+}, [])
+
+  return (
+    <div className="wrapper">
+    <Sidebar />
+    <main className="main">
+      {/* <News /> */}
+      <h1 className="page-title">University Information</h1>
+      <University />
+      <Tutors />
+    </main>
+  </div>
+  )
 }
 
-export default App;
+
--- src/components/University/University.jsx
+++ src/components/University/University.jsx
@@ -1,68 +1,61 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import Paper from '../Paper';
 import { HiPencilAlt, HiTrash } from 'react-icons/hi';
 
 import styles from './University.module.css';
 import Modal from 'components/Modal';
 
-class University extends Component {
-  constructor(props) {
-    super(props);
+export default function University(props) {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const imagePath = '/images/School.png';
 
-    this.state = {
-      isModalVisible: false,
-    };
+  function toggleModal() {
+    setIsModalVisible(!isModalVisible);
   }
-
-
-  toggleModal = () => {
-    this.setState({isModalVisible: !this.state.isModalVisible})
-  }
-
-  render() {
-    const imagePath = '/images/School.png';
-
-    return (
-      <section className="section">
-        <Modal isOpen={this.state.isModalVisible} handleClose={() => {this.toggleModal()}} header={"University"}>
-          Add edit form
-        </Modal>
-        <div className={styles.university}>
-          <div className={styles.info}>
-            <Paper>
-              {/* <img src={schoolImage} alt="School" /> */}
-              <img
-                className={styles.universityIcon}
-                src={`${process.env.PUBLIC_URL}${imagePath}`}
-                alt="School"
-              />
-              <p>university</p>
-              <h3 className={styles.title}>MIT</h3>
-              <div className={styles.controls}>
-                <button className="button-icon" onClick={this.toggleModal}>
-                  <HiPencilAlt />
-                </button>
-                <button className="button-icon">
-                  <HiTrash />
-                </button>
-              </div>
-            </Paper>
-          </div>
-
-          <div className={styles.description}>
-            Experience, a concentration of knowledge and the ability to avoid
-            most recruiting mistakes. We know what most local and foreign
-            companies want and we can give it to you. And we are constantly
-            improving our programming courses, adding something new there. You
-            can see the success stories of our alumni for yourself to see the
-            effectiveness of our teaching methodology. Yes, we will start with
-            the basics and the most basic information. We know that most people
-            come to us with zero knowledge.
-          </div>
+  return (
+    <section className="section">
+      <Modal
+        isOpen={isModalVisible}
+        handleClose={() => {
+          toggleModal();
+        }}
+        header={'University'}
+      >
+        Add edit form
+      </Modal>
+      <div className={styles.university}>
+        <div className={styles.info}>
+          <Paper>
+            {/* <img src={schoolImage} alt="School" /> */}
+            <img
+              className={styles.universityIcon}
+              src={`${process.env.PUBLIC_URL}${imagePath}`}
+              alt="School"
+            />
+            <p>university</p>
+            <h3 className={styles.title}>MIT</h3>
+            <div className={styles.controls}>
+              <button className="button-icon" onClick={toggleModal}>
+                <HiPencilAlt />
+              </button>
+              <button className="button-icon">
+                <HiTrash />
+              </button>
+            </div>
+          </Paper>
         </div>
-      </section>
-    );
-  }
-}
 
-export default University;
+        <div className={styles.description}>
+          Experience, a concentration of knowledge and the ability to avoid most
+          recruiting mistakes. We know what most local and foreign companies
+          want and we can give it to you. And we are constantly improving our
+          programming courses, adding something new there. You can see the
+          success stories of our alumni for yourself to see the effectiveness of
+          our teaching methodology. Yes, we will start with the basics and the
+          most basic information. We know that most people come to us with zero
+          knowledge.
+        </div>
+      </div>
+    </section>
+  );
+}
