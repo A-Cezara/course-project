7eaacd818c22c519b1b394b6e76de5da3091bfc6
A-Cezara <alexandracezara@yahoo.ro>
2024-09-16 12:01:03 +0300

    router basename

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/course-project/">
+    <BrowserRouter basename="course-project">
       <App />
     </BrowserRouter>
   </React.StrictMode>
