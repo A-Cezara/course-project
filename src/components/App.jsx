174680a546740400a8dd6c8c83b6a0b0843c143c
A-Cezara <alexandracezara@yahoo.ro>
2024-09-18 14:12:20 +0300

    lesson 11

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,21 +5,19 @@ import data from '../utils/data.json';
 import { useRef, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-import UniversityPage from 'pages/UniversityPage/UniversityPage';
-import FacultiesPage from 'pages/FacultiesPage/FacultiesPage';
-import FacultyContent from './FacultyContent/FacultyContent';
 import HistoryContent from './HistoryContent/HistoryContent';
 import DescriptionContent from './DescriptionContent/DescriptionContent';
 import NotFoundPage from 'pages/NotFoundPage/NotFoundPage';
 import LoadingPage from 'pages/LoadingPage/LoadingPage';
-import { FaRegFileCode } from 'react-icons/fa';
 
 const LazyUniversityPage = lazy(() =>
   import('pages/UniversityPage/UniversityPage')
 );
+
 const LazyFacultiesPage = lazy(() =>
   import('pages/FacultiesPage/FacultiesPage')
 );
+
 const LazyFacultyContent = lazy(() =>
   import('./FacultyContent/FacultyContent')
 );
@@ -35,7 +33,7 @@ export default function App() {
       <main className="main">
         <Suspense fallback={<LoadingPage />}>
           <Routes>
-            <Route path="/" element={<UniversityPage />} />
+            <Route path="/" element={<LazyUniversityPage />} />
             <Route path="/university" element={<LazyUniversityPage />} />
             <Route path="/faculties" element={<LazyFacultiesPage />} />
             <Route
